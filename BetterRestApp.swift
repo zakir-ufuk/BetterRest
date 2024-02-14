dd6f60fb0c63f3cbb61680c95ac89cd1e8698178
Zakir Ufuk Sahiner <z.ufuk.sahiner@gmail.com>
2024-02-14 11:19:30 +0100

    Initial commit

---
+++ BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Zakir Ufuk Sahiner on 03.02.24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
